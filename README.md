973fc69491ef8ba6d8f67d17ea6cdebfeddf5c01
Roman Birukov <r.v.birukov@yandex.ru>
2017-11-25 09:16:17 +0300

    изменил readme

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
     rewrite ^/create/(.*)$ /create_article.php break;     
     rewrite ^/create/([0-9]+)-([a-z0-9\-]+)?$ /create_article.php?id=$1 break;
     rewrite ^/([0-9]+)-([a-z0-9\-]+)/?$ /full_article.php?id=$1 break;
+
+SQL для таблицы: 
+
+		CREATE TABLE `test`. ( `article_id` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(500) NOT NULL , `notation` TEXT NOT NULL , `article` TEXT NOT NULL , `created` DATETIME NOT NULL , PRIMARY KEY (`article_id`)) ENGINE = InnoDB;
--- header.php
+++ header.php
@@ -7,7 +7,7 @@ require_once 'translit.php';
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="http://test.loc:8080/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="/css/styles.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
